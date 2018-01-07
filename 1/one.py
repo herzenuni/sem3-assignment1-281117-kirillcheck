cf81801c2716c672cb09f72b704836940e77f460
kirillcheck <32092781+kirillcheck@users.noreply.github.com>
2018-01-08 01:06:06 +0300

    Update one.py

--- 1/one.py
+++ 1/one.py
@@ -1,1 +1,20 @@
+# Задача 1
+# Шахов Кирилл
+ # 2 курс
+# Задача - Вычислите сумму цифр данного натурального числа
 
+def enter():  #ф. для ввода числа
+    number = int(input("Enter the number = "))
+    return number
+
+def sum_number(number):  #ф. для подсчета суммы чифр числа, на вход подается число, на выходе выдается сумма
+	number = str(number)
+	sum = 0
+	for ch in number:
+		sum = sum + int(ch)
+	return sum
+
+def out(number):
+	print("Sum of numbers {} = {}".format(number, sum_number(number)))
+
+out(sum_number(enter()))
